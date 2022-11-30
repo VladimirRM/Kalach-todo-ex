f8d8b6bb7c52dc0f9011c7e04609945dec2a4e6e
Vladimir RM <shvdrummer@gmail.com>
2022-11-30 14:06:46 +0200

    main import components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,4 @@
 import React, { useState } from "react";
+import ToDo from './ToDo'
 import ToDo from "./ToDo";
 import ToDoForm from "./ToDoForm";
-
-function App() {
-  const [todos, setTodos] = useState([]);
-
-  const addTask = () => {};
-  const removeTask = () => {};
-  const handleToggle = () => {};
-
-  return (
-    <div className="App">
-      <header>
-        <h1>Todo List{todos.length}</h1>
-      </header>
-      <ToDoForm/>
-      {todos.map((todo)=>{
-        return(
-          <ToDo
-          key={todo.id}
-          todo={todo}
-          toggleTask={handleToggle}
-          removeTask={removeTask}
-          />
-        )
-      })}
-
-    </div>
-  );
-}
-
-export default App;
