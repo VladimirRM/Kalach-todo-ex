4c5b684c77d4337c08bee3a387908fe69e0020ee
Vladimir RM <shvdrummer@gmail.com>
2022-11-30 14:19:55 +0200

    impot and create usestate in todoform

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React, { useState } from "react";
-import ToDo from './ToDo'
+import ToDo from "./ToDo";
 import ToDo from "./ToDo";
 import ToDoForm from "./ToDoForm";
 
+function App() {
+  const [todos, setTodos] = useState([]);
+  addTask = () => {};
+  removeTask = () => {};
+  handleToggle = () => {};
 
-function App(){
-    const [todos,setTodos] = useState([])
-   addTask=()=>{
-    
-   }
-   removeTask=()=>{
-
-   }
-   handleToggle=()=>{
-
-   }
-
-       return(
-        <div className="App">
+  return (
+    <div className="App">
+      <header>
+        <h1>Todo List:{todos.length}</h1>
+      </header>
+      <ToDoForm />
+      {todos.map((todo)=>{
+        return(
+          <ToDo
+          key={todo.id}
+          todo={todo}
+           removeTask={removeTask}
+           toggleTask={handleToggle}
           
+          />
+        )
 
-        </div>
-        
-       )
-
-
-
+      })}
+    </div>
+  );
 }
