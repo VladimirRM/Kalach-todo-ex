384073275774e781a936ad2ccd0bc15946572a86
Vladimir RM <shvdrummer@gmail.com>
2022-11-30 14:09:31 +0200

    create func App

--- src/App.js
+++ src/App.js
@@ -2,3 +2,17 @@ import React, { useState } from "react";
 import ToDo from './ToDo'
 import ToDo from "./ToDo";
 import ToDoForm from "./ToDoForm";
+
+
+function App(){
+  
+       return(
+        <div className="App">
+
+        </div>
+        
+       )
+
+
+
+}
