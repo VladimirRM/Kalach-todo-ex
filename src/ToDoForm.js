e1ed724ae5bc6d8dc739508b2a1a1fb2474b5317
Vladimir RM <shvdrummer@gmail.com>
2022-12-02 14:57:48 +0200

    add fom and input

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 
 const ToDoForm = () => {
   const [userInput,setUserInput]= useState('')
-  return <div></div>;
+  return (
+    <form  onSubmit={handleSubmit}>
+      <input type="text" />
+    </form>
+  )
 };
 
 export default ToDoForm;
