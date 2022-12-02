b18cafee17a1c2071ab81dcc9122bf8ac85c73dd
Vladimir RM <shvdrummer@gmail.com>
2022-12-02 15:25:34 +0200

    create main div in todo

--- src/ToDo.js
+++ src/ToDo.js
@@ -2,16 +2,11 @@ import React from "react";
 
 const ToDo = ({ toggleTask, todo, removeTask }) => {
   return (
-    <div key={todo.id} className="item-todo">
-      <div
-        className={todo.complete ? "item-text strike" : "item"}
-        onClick={() => toggleTask(todo.id)}
-      >
-        {todo.task}
-      </div>
-      <div className="item-delete" onClick={() => removeTask(todo.id)}></div>
-    </div>
-  );
-};
+<div className="item-todo" key={todo.id}>
+<div className={todo.complete? 'item-text strike':'item-text'} onClick>{todo.task}</div>
+<div className="item-delete">X</div>
+
+</div>
+  )
 
 export default ToDo;
