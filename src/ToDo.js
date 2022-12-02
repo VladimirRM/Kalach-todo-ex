14c4aca014b78594287075e193cedf623e816985
Vladimir RM <shvdrummer@gmail.com>
2022-12-02 15:29:01 +0200

    fin write todo div

--- src/ToDo.js
+++ src/ToDo.js
@@ -2,11 +2,18 @@ import React from "react";
 
 const ToDo = ({ toggleTask, todo, removeTask }) => {
   return (
-<div className="item-todo" key={todo.id}>
-<div className={todo.complete? 'item-text strike':'item-text'} onClick>{todo.task}</div>
-<div className="item-delete">X</div>
-
-</div>
-  )
+    <div className="item-todo" key={todo.id}>
+      <div
+        className={todo.complete ? "item-text strike" : "item-text"}
+        onClick={() => toggleTask(todo.id)}
+      >
+        {todo.task}
+      </div>
+      <div className="item-delete" onClick={() => removeTask(todo.id)}>
+        X
+      </div>
+    </div>
+  );
+};
 
 export default ToDo;
