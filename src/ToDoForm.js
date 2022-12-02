ae1915e7051749f06537f78f02a08f15accde1ea
Vladimir RM <shvdrummer@gmail.com>
2022-12-02 15:05:40 +0200

    add button save in input

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -1,28 +1,28 @@
 import React, { useState } from "react";
 
-const ToDoForm = () => {
-  const [userInput,setUserInput]= useState('')
- const handleChange =()=>{
-  
- }
- const handleSubmit =()=>{
-
- }
- 
-
-
+const ToDoForm = ({addTask}) => {
+  const [userInput, setUserInput] = useState("");
+  const handleChange = (e) => {
+    setUserInput(e.current.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask(userInput);
+  };
+  const handleKeyPress = () => {};
 
   return (
-    <form  onSubmit={handleSubmit}>
-      <input type="text"
-      value={userInput}
-      onChange={handleChange}
-      onKeyDown={handleKeyPress}
-      placeholder='Enter something...'
-      
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={userInput}
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+        placeholder="Enter something..."
       />
+      <button >Save</button>
     </form>
-  )
+  );
 };
 
 export default ToDoForm;
