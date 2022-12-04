05998f29bb9b5ece296603ffb08fb42ac91899cf
Vladimir RM <shvdrummer@gmail.com>
2022-12-04 09:26:19 +0200

    re comp todo

--- src/ToDo.js
+++ src/ToDo.js
@@ -2,17 +2,7 @@ import React from "react";
 
 const ToDo = ({ toggleTask, todo, removeTask }) => {
   return (
-    <div className="item-todo" key={todo.id}>
-      <div
-        className={todo.complete ? "item-text strike" : "item-text"}
-        onClick={() => toggleTask(todo.id)}
-      >
-        {todo.task}
-      </div>
-      <div className="item-delete" onClick={() => removeTask(todo.id)}>
-        X
-      </div>
-    </div>
+
   );
 };
 
