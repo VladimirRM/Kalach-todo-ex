f03f97939070042395e18a6c9460a0c23dd81da5
Vladimir RM <shvdrummer@gmail.com>
2022-12-04 09:31:51 +0200

    add func toggleTask

--- src/ToDo.js
+++ src/ToDo.js
@@ -4,8 +4,8 @@ const ToDo = ({ toggleTask, todo, removeTask }) => {
   return (
   
   <div className="item-todo">
-    <div></div>
-    <div className="item-delete" ></div>
+    <div  onClick={()=>toggleTask(todo.id)}>{todo.task}</div>
+    <div className="item-delete" >X</div>
   </div>
 
   );
