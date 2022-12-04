2beeb0c1dcd2046d0b964c76f0751062a3cc4847
Vladimir RM <shvdrummer@gmail.com>
2022-12-04 09:28:51 +0200

    add div className="item-todo

--- src/ToDo.js
+++ src/ToDo.js
@@ -2,6 +2,11 @@ import React from "react";
 
 const ToDo = ({ toggleTask, todo, removeTask }) => {
   return (
+  
+  <div className="item-todo">
+    <div></div>
+    <dir></dir>
+  </div>
 
   );
 };
