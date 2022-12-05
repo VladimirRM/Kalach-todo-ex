c03951918befe5d4e307eae8134a5f05e15cc7ae
Vladimir RM <shvdrummer@gmail.com>
2022-12-05 10:54:32 +0200

    create input & button save

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -1,14 +1,23 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-const ToDoForm = ({addTask}) => {
-  const [userInput,setUserInput]=useState('')
+const ToDoForm = ({ addTask }) => {
+  const [userInput, setUserInput] = useState("");
 
-   const handleSubmit
+  const handleSubmit = () => {};
+  const handleChange = () => {};
+  const handleKeyPress = () => {};
   return (
     <form onSubmit={handleSubmit}>
-
+      <input
+        type="text"
+        onChange={handleChange}
+        value={userInput}
+        onKeyDown={handleKeyPress}
+        placeholder="Enter something"
+      />
+      <button>Save</button>
     </form>
-  )
-}
+  );
+};
 
-export default ToDoForm
+export default ToDoForm;
