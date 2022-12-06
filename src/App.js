2c6ba738f926ea9a8cf69ab18a731adb46635707
Vladimir RM <shvdrummer@gmail.com>
2022-12-06 11:35:11 +0200

    fin main import

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react'
+import ToDo from './ToDo'
+import ToDoForm from './ToDoForm'
+import "./index.css"
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
