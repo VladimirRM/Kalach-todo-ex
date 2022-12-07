db2d0a9f557da2c180f79fef30f6c9f1cbdacb2b
Vladimir RM <shvdrummer@gmail.com>
2022-12-07 10:13:13 +0200

    add   addTask(userInput) in handlesubmit

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -3,11 +3,13 @@ import React, { useState } from "react";
 const ToDoForm = ({addTask}) => {
   const [userInput, setUserInput] = useState("");
 
-  const handleChange =()=>{
+  const handleChange =(e)=>{
+    setUserInput(e.currentTarget.value)
 
   }
-  const handleSubmit =()=>{
-    
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+    addTask(userInput)
   }
   const handleKeyPress =()=>{
 
