f7579303d7fe2d0ee58e2ea707bb1da761e2e96a
Vladimir RM <shvdrummer@gmail.com>
2022-12-07 10:15:42 +0200

    create addtask components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import React,{useState} from 'react'
-import ToDo from './ToDo'
-import ToDoForm from './ToDoForm'
-import './index.css'
+import React, { useState } from "react";
+import ToDo from "./ToDo";
+import ToDoForm from "./ToDoForm";
+import "./index.css";
 
 const App = () => {
-const [todos,setTodos]= useState([])
+  const [todos, setTodos] = useState([]);
 
-
-const addTask =()=>{
-  
-}
-const removeTask =()=>{
-
-}
-const handleToggle =()=>{
-
-}
-
+  const addTask = (userInput) => {
+    if (userInput) {
+      const newItem = {
+        id: Math.random().toString(36).substring(2, 9),
+        task: userInput,
+        complete: false,
+      };
+      setTodos([...todos, newItem]);
+    }
+  };
+  const removeTask = () => {};
+  const handleToggle = () => {};
 
   return (
-    <div className='App'>
+    <div className="App">
       <header>
         <h1>Todo List:{todos.length}</h1>
       </header>
-      <ToDoForm addTask={addTask}/>
-      {todos.map((todo)=>{
-        return(
+      <ToDoForm addTask={addTask} />
+      {todos.map((todo) => {
+        return (
           <ToDo
-          key={todo.id}
-          todo={todo}
-          removeTask={removeTask}
-          toggleTask={handleToggle}
+            key={todo.id}
+            todo={todo}
+            removeTask={removeTask}
+            toggleTask={handleToggle}
           />
-        )
+        );
       })}
-      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
