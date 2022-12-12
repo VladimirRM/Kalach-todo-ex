7a9e6dda590e6312099616ae14c7904de06572a6
Vladimir RM <shvdrummer@gmail.com>
2022-12-12 11:53:15 +0200

    create toggle task in todo

--- src/ToDo.js
+++ src/ToDo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ToDo = ({todo,removeTask,toggleTask}) => {
   return (
     <div className='item-todo' key={todo.id}>
-      <div></div>
+      <div  className={todo.complete?'item-text strike':'item-text'} onClick={()=>toggleTask(todo.id)}></div>
       <div className="item-delete" onClick={()=>removeTask(todo.id)}>X</div>
     </div>
   )
