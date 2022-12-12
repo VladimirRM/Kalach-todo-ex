29c6bb94b4c35579325109d3031d1511a7380224
Vladimir RM <shvdrummer@gmail.com>
2022-12-12 11:50:35 +0200

    add func remove task in todo

--- src/ToDo.js
+++ src/ToDo.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const ToDo = ({todo,removeTask,toggleTask}) => {
   return (
-    <div>
-      
+    <div className='item-todo' key={todo.id}>
+      <div></div>
+      <div className="item-delete" onClick={()=>removeTask(todo.id)}>X</div>
     </div>
   )
 }
